c62d5956d208257762e2def8421ff67d9c88cc94
Oks123321 <81802094+Oks123321@users.noreply.github.com>
2022-02-22 23:50:20 +0200

    Added Task#1

---
+++ src/com/company/MyOwnFileReader.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MyOwnFileReader {
+
+    public static void main(String args[]) throws IOException {
+        Scanner scanner = new Scanner(new FileInputStream("file.txt"));
+
+        while (scanner.hasNextLine()) {
+            String telNumber = scanner.nextLine();
+            Pattern pattern = Pattern.compile("^[0-9]{3}-[0-9]{3}-[0-9]{4}$");
+            Pattern pattern1 = Pattern.compile("^\\([0-9]{3}\\) [0-9]{3}-[0-9]{4}$");
+
+            Matcher matcher = pattern.matcher(telNumber);
+            Matcher matcher1 = pattern1.matcher(telNumber);
+
+            if (matcher.matches()) {
+                System.out.println(telNumber);
+            }
+            if (matcher1.matches()) {
+                System.out.println(telNumber);
+            }
+        }
+    }
+}
+
+
+
+
+
