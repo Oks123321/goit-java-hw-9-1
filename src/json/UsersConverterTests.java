08c031f2fad682939f73137ed843dfbc85b48b2f
Oks123321 <81802094+Oks123321@users.noreply.github.com>
2022-04-01 22:44:09 +0300

    Added file .gitignore, added library, delete comments

--- src/json/UsersConverterTests.java
+++ src/json/UsersConverterTests.java
@@ -3,7 +3,7 @@ package json;
 public class UsersConverterTests {
     public static final String USERS_FILE_FROM = "src/file1.txt";
     public static final String USERS_FILE_TO = "src/file1.json";
-//"./src/main/java/goit/hw_9_2/file.txt"
+
     public static void main(String[] args) {
         new UsersConverter().convert(USERS_FILE_FROM, USERS_FILE_TO);
     }
